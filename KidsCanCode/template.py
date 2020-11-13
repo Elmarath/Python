eca31d97e52116f4b9502588e9fa887bedbe3e1e
Elmarath <65414809+Elmarath@users.noreply.github.com>
2020-11-13 18:57:12 +0300

    dog and cad

--- KidsCanCode/template.py
+++ KidsCanCode/template.py
@@ -12,10 +12,10 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 150, 0)
 BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
 
 # set up assets folders
 game_folder = os.path.dirname(__file__)
-print(game_folder)
 img_folder = os.path.join(game_folder, "images")
 
 
@@ -24,12 +24,18 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         # idk what this do but required
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 40))
-        self.image.fill(GREEN)
+        self.image = pygame.image.load(
+            os.path.join(img_folder, "MuscleDoge.png")).convert()
+        self.image = pygame.transform.scale(self.image, (64, 64))
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
         self.speedx = 0
+
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
 
     def update(self):
         self.speedx = 0
@@ -49,23 +55,45 @@ class Mob(pygame.sprite.Sprite):
     # sprite for enemies
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((30, 40))
-        self.image.fill(RED)
+        self.images = (os.path.join(img_folder, "YellingCad.png"), os.path.join(
+            img_folder, "CryingCad.png"), os.path.join(img_folder, "BrokenCad.jpg"))
+        self.RandImageIndex = random.randrange(0, 3)
+        self.image = pygame.image.load(
+            self.images[self.RandImageIndex]).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (32, 32))
         self.rect = self.image.get_rect()
         self.rect.x = random.randrange(0, WIDTH - self.rect.width)
         self.rect.y = random.randrange(-100, -40)
+        self.speedx = random.random()*6 - 3
         self.speedy = random.randrange(3, 8)
 
     def update(self):
+        self.rect.x += self.speedx
         self.rect.y += self.speedy
         # if not in gameplay
-        if self.rect.top > HEIGHT + 10:
+        if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 25:
             # spawn again
             self.rect.x = random.randrange(0, WIDTH - self.rect.width)
             self.rect.y = random.randrange(-100, -40)
             self.speedy = random.randrange(3, 8)
 
 
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join(
+            img_folder, "NoMuscleDoge.jpg")).convert()
+        self.image = pygame.transform.scale(self.image, (16, 16))
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self. speedy = -10
+
+    def update(self):
+        self.rect.y += self.speedy
+        # kill if it reaches top of the screen
+        if self.rect.bottom < 0:
+            self.kill()
 # refresh window
 
 
@@ -82,9 +110,10 @@ clock = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
 mobs = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
-for i in range(0, 8):
+for i in range(0, 16):
     m = Mob()
     all_sprites.add(m)
     mobs.add(m)
@@ -98,9 +127,25 @@ while run:
         # check for closure
         if event.type == pygame.QUIT:
             run = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shoot()
+            if event.key == pygame.K_ESCAPE:
+                run = False
     # Update
     all_sprites.update()
-    #Draw / render
+    # check to see if a bullet hit a mob
+    hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
+    for hit in hits:
+        m = Mob()
+        all_sprites.add(m)
+        mobs.add(m)
+    # check to see if a mob hit the player
+    # returns: [<Mob sprite(in 2 groups)>, <Mob sprite(in 2 groups)>]
+    hits = pygame.sprite.spritecollide(player, mobs, False)
+    if hits:
+        run = False
+    # Draw / render
     screen.fill((210, 200, 210))
     all_sprites.draw(screen)
 
